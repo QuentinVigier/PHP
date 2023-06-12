ae21224cbedaad49903c018480ca3fbd74d75836
QuentinVigier <quentin.vigier12@gmail.com>
2023-06-12 16:59:56 +0200

    Update PHP-connexion

--- Exo-PHP-Formulaire/exo-php-form.php
+++ Exo-PHP-Formulaire/exo-php-form.php
@@ -39,5 +39,24 @@
 
     $bdd->exec("INSERT INTO client(civilite, nom, prenom, ddn, commune, phone, mail, web, niveau, lgge) values ('".$civilite."', '".$nom."', '".$prenom."', '".$ddn."', '".$commune."',
     '".$telephone."', '".$mail."', '".$web."', '".$niveau."', '".$lgge."')");
+
+
+    $req = $bdd->prepare("SELECT idClient, nom, prenom FROM client");
+    $req->execute();
+    $taille = $req->rowCount();
+    echo "<table border>";
+    if($taille == 0){
+        echo "Désolé nous n'avons rien à afficher pour l'instant";
+    }else{
+        echo "<tr><th>Numéro client</th><th>Nom</th><th>Prénom</th></tr>";
+    }
+    while($d = $req->fetch()){
+        echo "<tr> <td>" . $d[0] . "</td><td>" . $d[1] . "</td><td>" . $d[2] . "</td></tr>";
+    }
+    
+    echo "</table>";
+
+;
+
 // }
 
